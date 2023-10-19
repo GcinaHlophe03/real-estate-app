e703cd785501c3c572e8617f07f6e014f9b1e7cc
Gcina Hlophe <99721134+GcinaHlophe03@users.noreply.github.com>
2023-10-19 14:59:08 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
   }
 }
 
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  basePath: '', 
-  
+  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
 };
